1712c6e3a0d30272684691f6d3694b33a8c87d57
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-19 14:06:11 +0300

    Register the Profile class.

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment, Rating
+from .models import Post, Comment, Rating,Profile
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Rating)
+admin.site.register(Profile)
